f62bb27a56cfcdc29b513dbde8a2cf449e3d75cc
ewasawczynska <sawczynska.e@gmail.com>
2023-06-12 18:05:06 +0200

    task1/gallery-done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,11 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+const gallery = galleryItems
+  .map(
+    (galleryItem) =>
+      `<li><div class="gallery__item"><a class="gallery__link" href="${galleryItem.original}"><img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}"/></a></div></li>`
+  )
+  .join("");
+const pageGallery = document.querySelector(".gallery");
+pageGallery.insertAdjacentHTML("beforeend", gallery);
 console.log(galleryItems);
