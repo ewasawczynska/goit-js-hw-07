e3b75b3dc665c77a5dba035ba1e4afa122efe22e
ewasawczynska <sawczynska.e@gmail.com>
2023-06-14 21:21:35 +0200

    task1 - done without escape

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,29 +1,31 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 console.log(galleryItems);
+const pageGallery = document.querySelector(".gallery");
 
 const gallery = galleryItems
   .map(
     (galleryItem) =>
-      `<li><div class="gallery__item"><a class="gallery__link" href="${galleryItem.original}"><img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}"/></a></div></li>`
+      `<div class="gallery__item"><a class="gallery__link" href="${galleryItem.original}"><img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}"/></a></div>`
   )
   .join("");
-const pageGallery = document.querySelector(".gallery");
+
 pageGallery.insertAdjacentHTML("beforeend", gallery);
 
-pageGallery.addEventListener("click", selectImage);
-function selectImage(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== "IMG") {
-    return;
-  }
-}
-
-const instance = basicLightbox.create(`
-    <div class="gallery__item">
-    <a class="gallery__link" href="${galleryItems.original}"><img class="gallery__image" src="${galleryItems.original}" data-source="${galleryItems.original}" alt="${galleryItems.description}"/></a>
-    </div>
-`);
-
-instance.show();
-const visible = instance.visible();
+pageGallery.addEventListener("click", (e) => {
+  e.preventDefault();
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}">`,
+    {
+      onClose: (instance) => {
+        document.addEventListener("keydown", escapeFunc);
+      },
+    }
+  );
+  instance.show();
+  const escapeFunc = (e) => {
+    if (e.key === "Escape") {
+      instance.close();
+    }
+  };
+});
